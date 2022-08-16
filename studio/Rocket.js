4218a2ebd2329cffffb72e021c29e3df36471f4c
Kevin Peery <kevin.peery@gmail.com>
2022-08-15 23:05:27 -0500

    studio complete

---
+++ studio/Rocket.js
@@ -0,0 +1,40 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumWeight(this.cargoItems) + this.sumWeight(this.astronauts);
+    };
+    Rocket.prototype.sumWeight = function (items) {
+        var total = 0;
+        for (var i = 0; i < items.length; i++) {
+            total += items[i].massKg;
+        }
+        return total;
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
